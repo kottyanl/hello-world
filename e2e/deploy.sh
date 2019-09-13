543706cdc0611580184e3c0ab9e43dad34ab0454
Laszlo Kottyan <kottyan.laszlo@balasys.hu>
2019-09-13 15:42:58 +0200

    change ci structure

--- e2e/deploy.sh
+++ e2e/deploy.sh
@@ -3,5 +3,7 @@ set -e
 
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE}")
 cd $SCRIPT_ROOT
+
 docker build -t python-http:1.0.0 .
 kubectl create -f deployments.yaml
+kubectl get deployments
--- e2e/test.sh
+++ e2e/test.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
 set -e
+
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE}")
+cd $SCRIPT_ROOT
+
+echo "Testing"
