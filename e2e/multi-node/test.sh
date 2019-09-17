05b5b6e017e289172bcd16f906cecfe84f74b6de
Laszlo Kottyan <kottyan.laszlo@balasys.hu>
2019-09-17 15:41:53 +0200

    test

--- e2e/multi-node/test.sh
+++ e2e/multi-node/test.sh
@@ -4,27 +4,23 @@
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE}")
 cd $SCRIPT_ROOT
 
-echo $(minikube ip)
+test_request () {
+    echo $(docker exec -it kind-control-plane curl http://localhost:$1) >> results.txt
+}
 
-curl http://$(minikube ip)
+rm -f results.txt 
 
-# test_request () {
-#     echo $(docker exec -it kind-control-plane curl http://localhost:$1) >> results.txt
-# }
+# Test apps listening on different ports
+test_request 30500
+test_request 30400
+test_request 30300
 
-# rm -f results.txt 
+cat results.txt
 
-# # Test apps listening on different ports
-# test_request 30500
-# test_request 30400
-# test_request 30300
-
-# cat results.txt
-
-# # Check and set exit status
-# while read p; do
-#   if [[ "$p" != "Hello"* ]]; then
-#     exit 1
-#   fi
-# done <results.txt
-# exit 0
+# Check and set exit status
+while read p; do
+  if [[ "$p" != "Hello"* ]]; then
+    exit 1
+  fi
+done <results.txt
+exit 0
--- e2e/single-node/test.sh
+++ e2e/single-node/test.sh
@@ -4,23 +4,28 @@
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE}")
 cd $SCRIPT_ROOT
 
-test_request () {
-    echo $(docker exec -it minikube curl http://localhost:$1) >> results.txt
-}
 
-rm -f results.txt 
+echo $(minikube ip)
 
-# Test apps listening on different ports
-test_request 30500
-test_request 30400
-test_request 30300
+curl http://$(minikube ip)
 
-cat results.txt
+# test_request () {
+#     echo $(docker exec -it minikube curl http://localhost:$1) >> results.txt
+# }
 
-# Check and set exit status
-while read p; do
-  if [[ "$p" != "Hello"* ]]; then
-    exit 1
-  fi
-done <results.txt
-exit 0
+# rm -f results.txt 
+
+# # Test apps listening on different ports
+# test_request 30500
+# test_request 30400
+# test_request 30300
+
+# cat results.txt
+
+# # Check and set exit status
+# while read p; do
+#   if [[ "$p" != "Hello"* ]]; then
+#     exit 1
+#   fi
+# done <results.txt
+# exit 0
