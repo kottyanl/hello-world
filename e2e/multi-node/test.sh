9ce9a039c45358480c0f36c2feb8f32fa8f7d668
Laszlo Kottyan <kottyan.laszlo@balasys.hu>
2019-09-18 16:12:17 +0200

    test

--- e2e/multi-node/test.sh
+++ e2e/multi-node/test.sh
@@ -4,36 +4,30 @@
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE}")
 cd $SCRIPT_ROOT
 
-curl -fisS http://localhost:30500
+test_request () {
+    curl -fisS http://localhost:$1 >> log 2>>err.log
+    echo -e '\n' >> log
+}
 
+rm -f log err.log 
 
-# test_request () {
-#     docker exec -it kind-control-plane curl -fisS http://localhost:$1 >> log 2>>err.log
-#     docker exec -it kind-control-plane echo -e '\n' >> log
-# }
+# Test apps listening on different ports
+echo -e '\nTesting started'
 
-# docker exec -it kind-control-plane rm -f log err.log 
+test_request 30500
+test_request 30400
+test_request 30300
 
-# # Test apps listening on different ports
-# echo -e '\nTesting started'
+echo -e '\nTesting finished'
 
-# test_request 30500
-# test_request 30400
-# test_request 30300
+if [ -s "log" ]; then
+  echo -e "\nRESPONSES:\n"
+  cat log
+fi
 
-# echo -e '\nTesting finished'
+if [ -s "err.log" ]; then
+  echo -e "\nERRORS:\n"
+  cat err.log
+fi
 
-# docker exec -it kind-control-plane test -s "log"
-# if [ $(echo $?) -eq 0 ]; then
-#   echo -e "\nRESPONSES:\n"
-#   docker exec -it kind-control-plane cat "log"
-# fi
-
-# docker exec -it kind-control-plane test -s "err.log"
-# if [ $(echo $?) -eq 0 ]; then
-#   echo -e "\nERRORS:\n"
-#   docker exec -it kind-control-plane cat "err.log"
-# fi
-
-# docker exec -it kind-control-plane test -s "err.log"
-# [ $(echo $?) -eq 0 ] && exit 1 || exit 0 
+[ -s err.log ] && exit 1 || exit 0 
