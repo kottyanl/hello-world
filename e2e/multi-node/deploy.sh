af4e897f5d66c68ec1093c4134718fddc7436365
Laszlo Kottyan <kottyan.laszlo@balasys.hu>
2019-09-18 12:44:02 +0200

    change

--- e2e/multi-node/deploy.sh
+++ e2e/multi-node/deploy.sh
@@ -6,8 +6,8 @@ cd $SCRIPT_ROOT
 
 docker build -t python-http:1.0.1 ..
 docker images
-kind load docker-image python-http:1.0.1
-kind load docker-image python:3.7
+kind load docker-image python-http:1.0.1 --loglevel trace
+# kind load docker-image python:3.7
 # kubectl apply -f deployments.yaml
 # kubectl apply -f services.yaml
 # kubectl wait --for=condition=available --timeout=600s deployment/http-app-1
