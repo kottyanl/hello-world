2c31c4f76edd3dbef3ebdb28bc05ff8d6e786c46
Laszlo Kottyan <kottyan.laszlo@balasys.hu>
2019-09-18 08:36:12 +0200

    change test

--- e2e/single-node/test.sh
+++ e2e/single-node/test.sh
@@ -4,48 +4,18 @@
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE}")
 cd $SCRIPT_ROOT
 
-kubectl get pods -o wide
+test_request () {
+    curl -fisS http://$(minikube ip):$1 >>log 2>>err.log
+}
 
-sleep 30
+rm -f log err.log 
 
-kubectl get pods -o wide
+# Test apps listening on different ports
+test_request 30500
+test_request 30400
+test_request 30300
 
-sleep 30
+cat log 2>/dev/null
+cat err.log 2>/dev/null
 
-kubectl get pods -o wide
-
-echo $(minikube ip)
-
-curl http://$(minikube ip):30500
-
-curl http://$(minikube ip):30400
-
-echo 'DONE'
-
-
-# curl http://localhost
-# curl http://localhost:30500
-# docker exec -it kind-control-plane curl http://localhost:30500
-
-# docker exec -it kind-control-plane curl -v http://localhost:30500
-
-# test_request () {
-#     echo $(docker exec -it kind-control-plane curl http://127.0.0.1:$1) >> results.txt
-# }
-
-# rm -f results.txt 
-
-# # Test apps listening on different ports
-# test_request 30500
-# test_request 30400
-# test_request 30300
-
-# cat results.txt
-
-# # Check and set exit status
-# while read p; do
-#   if [[ "$p" != "Hello"* ]]; then
-#     exit 1
-#   fi
-# done <results.txt
-# exit 0
+[ -s err.log ] && exit 1 || exit 0 
